b1aed3f2b63049dde75f05d05bbe6d139bd6e361
Jhon Calderón <jhoncv@digitalware.com.co>
2020-05-05 19:23:19 -0500

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,47 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { environment } from '../environments/environment';
+
+// Ngrx
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { appReducers } from './app.reducer';
+
+// AngularFire
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
+// Modulos
+import { AppRoutingModule } from './app-routing.module';
+
+
 import { AppComponent } from './app.component';
+
+// Módulos
+import { AuthModule } from './auth/auth.module';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AuthModule,
+
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    AngularFireAuthModule,
+    
+    StoreModule.forRoot( appReducers ),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+      logOnly: environment.production, // Restrict extension to log-only mode
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyC7_iefRHjCaYYAXs5ZT7ionaZDG9WbEc8',
+    authDomain: 'ingreso-egreso-app-e1028.firebaseapp.com',
+    databaseURL: 'https://ingreso-egreso-app-e1028.firebaseio.com',
+    projectId: 'ingreso-egreso-app-e1028',
+    storageBucket: 'ingreso-egreso-app-e1028.appspot.com',
+    messagingSenderId: '615419961545',
+    appId: '1:615419961545:web:15f6e7e46c6dfcd87dad18',
+    measurementId: 'G-FLM5XK0DTR'
+  }
 };
